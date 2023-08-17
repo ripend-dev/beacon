3d3dc862954b1e4663a50c3a0dc9ece7805f956e
Ripend <142525915+ripend-dev@users.noreply.github.com>
2023-08-17 22:28:03 +0200

    Add files via upload

---
+++ beacon/core/commands.lua
@@ -0,0 +1,13 @@
+local kah, success = loadstring(game:HttpGet'https://raw.githubusercontent.com/szcvak/lua/main/Kohl\'s%20Admin%20House/Stellar/API.lua')()({
+    ScriptName = 'beacon',
+    Prefix = '#'
+})
+
+local cmds = {}
+
+cmds.goto = {
+    description = 'Walks to the targeted position',
+    command = function(pos)
+        
+    end
+}
